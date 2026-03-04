b98c982a7f2a999b5698d65b9db99f557cb349c1
yabluninn <yablunin.artem@gmail.com>
2026-03-04 18:10:15 +0200

    implement "profile" page with username param

--- src/app/(public)/explore/page.tsx
+++ src/app/(public)/explore/page.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
+import Explore from "./Explore";
 
 export const metadata: Metadata = {
   title: "Explore",
 };
 
-export default function Explore() {
+export default function ExplorePage() {
   return (
     <Suspense>
       <Explore />
--- src/app/(public)/u/[username]/page.tsx
+++ src/app/(public)/u/[username]/page.tsx
@@ -1,9 +1,21 @@
 import type { Metadata } from "next";
 
+type Params = { username: string };
+
 export const metadata: Metadata = {
   title: "Profile",
 };
 
-export default function Profile() {
-  return <div>Profile</div>;
+export default async function ProfilePage({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
+  const { username } = await params;
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Profile @{username}</h1>
+    </div>
+  );
 }
