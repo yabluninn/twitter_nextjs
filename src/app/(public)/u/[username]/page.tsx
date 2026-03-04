30a041fca21ec2d45061d6b2b8a84ffb3bb6ac88
yabluninn <yablunin.artem@gmail.com>
2026-03-04 18:40:43 +0200

    implement generateMetadata for better SEO

--- src/app/(public)/u/[username]/page.tsx
+++ src/app/(public)/u/[username]/page.tsx
@@ -2,9 +2,17 @@ import type { Metadata } from "next";
 
 type Params = { username: string };
 
-export const metadata: Metadata = {
-  title: "Profile",
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<Params>;
+}): Promise<Metadata> {
+  return { title: "@" + (await params).username };
+}
+
+// export const metadata: Metadata = {
+//   title: "Profile",
+// };
 
 export default async function ProfilePage({
   params,
@@ -12,7 +20,6 @@ export default async function ProfilePage({
   params: Promise<Params>;
 }) {
   const { username } = await params;
-
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Profile @{username}</h1>
